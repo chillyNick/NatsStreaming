79670b6d86ab852f0b0e3aeed6f0f130954a7dc0
Nickolay Mizenko <mizenko1998@gmail.com>
2022-07-08 08:41:01 +0300

    add rwLock to map

--- internal/cache/memory_cache.go
+++ internal/cache/memory_cache.go
@@ -1,19 +1,26 @@
 package cache
 
+import "sync"
+
 type memoryCache struct {
-	m map[string][]byte
+	m       map[string][]byte
+	rwMutex *sync.RWMutex
 }
 
 func NewMemoryCache() Cache {
-	return memoryCache{m: make(map[string][]byte)}
+	return memoryCache{m: make(map[string][]byte), rwMutex: &sync.RWMutex{}}
 }
 
 func (m memoryCache) Get(key string) ([]byte, bool) {
+	m.rwMutex.RLock()
 	data, ok := m.m[key]
+	m.rwMutex.RUnlock()
 
 	return data, ok
 }
 
 func (m memoryCache) Set(key string, data []byte) {
+	m.rwMutex.Lock()
 	m.m[key] = data
+	m.rwMutex.Unlock()
 }
